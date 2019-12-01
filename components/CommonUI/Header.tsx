ef67a7f9d22a19e89ad720567a1dddcbf030f398
Changhun Lee <wakie9292@gmail.com>
2019-12-01 23:09:29 +0900

    responsive header

--- components/CommonUI/Header.tsx
+++ components/CommonUI/Header.tsx
@@ -1,47 +1,82 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import { ROUTES } from '../../lib/Routes/Routes';
+import { colors } from '../../lib/styles/global';
+import Hamburger from './Hamburger';
+import { breakpoints } from '../../lib/styles/responsive';
 
-const Header = () => (
-  <Wrapper>
-    <div className="nav-box">
-      <Link href={ROUTES.home}>
-        <a>
-          <span className="blog-name">건망증 개발자 기록지</span>
-        </a>
-      </Link>
-      <nav className="nav">
-        <Link href={ROUTES.about}>
+export default function Home() {
+  const [toggle, setToggle] = useState(false);
+  return (
+    <Wrapper aColor={colors.violet9} breakpoints={breakpoints}>
+      <div className="nav-box">
+        <Link href={ROUTES.home}>
           <a>
-            <span>About</span>
+            <span className="blog-name">건망증 개발자 기록지</span>
           </a>
         </Link>
-        <Link href={ROUTES.devBlog}>
-          <a>
-            <span>Development</span>
-          </a>
-        </Link>
-        <Link href={ROUTES.about}>
-          <a>
-            <span>GitHub</span>
-          </a>
-        </Link>
-        <Link href={ROUTES.about}>
-          <a>
-            <span>Facebook</span>
-          </a>
-        </Link>
-      </nav>
-    </div>
-  </Wrapper>
-);
-
-const Wrapper = styled.header`
-  width: 90%;
+        <SmallNav toggle={toggle}>
+          <Link href={ROUTES.about}>
+            <a>
+              <span>About</span>
+            </a>
+          </Link>
+          <Link href={ROUTES.devBlog}>
+            <a>
+              <span>Development</span>
+              <ul className="bolg-category">
+                <li>Web</li>
+                <li>Javascript</li>
+                <li>React</li>
+                <li>Node</li>
+                <li>Html/css</li>
+              </ul>
+            </a>
+          </Link>
+          <Link href={ROUTES.write}>
+            <a>
+              <span>GitHub</span>
+            </a>
+          </Link>
+          <Link href={ROUTES.about}>
+            <a>
+              <span>Facebook</span>
+            </a>
+          </Link>
+        </SmallNav>
+        <Hamburger toggle={toggle} onToggle={() => setToggle(!toggle)} />
+        <nav className="nav">
+          <Link href={ROUTES.about}>
+            <a>
+              <span>About</span>
+            </a>
+          </Link>
+          <Link href={ROUTES.devBlog}>
+            <a>
+              <span>Development</span>
+            </a>
+          </Link>
+          <Link href={ROUTES.write}>
+            <a>
+              <span>GitHub</span>
+            </a>
+          </Link>
+          <Link href={ROUTES.about}>
+            <a>
+              <span>Facebook</span>
+            </a>
+          </Link>
+        </nav>
+      </div>
+    </Wrapper>
+  );
+}
+const Wrapper = styled.header<{ aColor: string; breakpoints: object }>`
+  width: calc(100% - 10rem);
   height: 6rem;
   padding-top: 10rem;
   margin: auto;
-  /* background-color: #ced4da; */
   border-bottom: 1px solid #ced4da;
   display: flex;
   align-items: center;
@@ -49,6 +84,7 @@ const Wrapper = styled.header`
     width: 100%;
     display: flex;
     justify-content: space-between;
+    align-items: center;
     .blog-name {
       font-size: 2rem;
       font-weight: 900;
@@ -58,12 +94,46 @@ const Wrapper = styled.header`
       justify-content: flex-end;
       a {
         font-size: 2rem;
-        color: #5f3dc4;
+        color: ${props => props.aColor};
         & + a {
           margin-left: 1rem;
         }
       }
+      @media screen and (max-width: ${breakpoints.small}) {
+        display: none;
+      }
     }
   }
 `;
-export default Header;
+
+const SmallNav = styled.aside<{ toggle: boolean }>`
+  display: flex;
+  flex-direction: column;
+  visibility: ${props => (props.toggle ? 'visible' : 'hidden')};
+  position: fixed;
+  background-color: black;
+  opacity: 0.8;
+  width: 30rem;
+  height: 100%;
+  right: 0;
+  top: 0;
+  z-index: 888;
+  transition: all 500ms;
+  transform: ${props =>
+    props.toggle ? 'translateX(0rem)' : 'translateX(35rem)'};
+  padding-top: 20rem;
+  padding-left: 5rem;
+  a {
+    color: white !important;
+    width: 80%;
+    .bolg-category {
+      font-size: 2rem;
+      padding-left: 3rem;
+    }
+  }
+  font-size: 3rem;
+  @media screen and (max-width: ${breakpoints.small}) {
+    transform: ${props => (props.toggle ? 'translateX()' : 'translateX(100%)')};
+    width: calc(100% - 5rem);
+  }
+`;
