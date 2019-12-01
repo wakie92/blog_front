ffe4b30e3d33dc457a465da9d811b5c6376faabf
Changhun Lee <wakie9292@gmail.com>
2019-12-01 23:12:01 +0900

    division of blog posts

---
+++ pages/dev/blog.tsx
@@ -0,0 +1,12 @@
+import { PostsLayout } from '../../components/Posts';
+
+export default function blog() {
+  return (
+    <>
+    <h1>
+      Development(전체글)
+    </h1>
+    <PostsLayout />
+    </>
+  )
+};
