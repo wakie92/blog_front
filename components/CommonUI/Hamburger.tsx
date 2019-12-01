7eec4ae2956bd3dde5396919524548a5d946a4e5
Changhun Lee <wakie9292@gmail.com>
2019-12-01 23:08:47 +0900

    Hamburger UI

---
+++ components/CommonUI/Hamburger.tsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+import { breakpoints } from '../../lib/styles/responsive';
+import { colors } from '../../lib/styles/global';
+
+interface HamburgerProps {
+  toggle: boolean;
+  onToggle: () => void;
+}
+export default function Hamburger({ toggle, onToggle }: HamburgerProps) {
+  return (
+    <BurgerIcon
+      colors={colors}
+      breakpoints={breakpoints}
+      toggle={toggle}
+      onClick={onToggle}
+    >
+      <div className="burger-line" />
+      <div className="burger-line" />
+      <div className="burger-line" />
+    </BurgerIcon>
+  );
+}
+
+const BurgerIcon = styled.button<{
+  colors: object;
+  breakpoints: object;
+  toggle: boolean;
+}>`
+  position: fixed;
+  top: 2rem;
+  right: 5rem;
+  width: 5rem;
+  height: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: ${colors.defaultBlack};
+  opacity: 0.8;
+  z-index: 900;
+  cursor: pointer;
+  &:hover {
+    background-color: ${colors.violet9};
+  }
+  transition: all 500ms;
+  transform: ${props =>
+    props.toggle ? 'translateX(-35rem)' : 'translateX(0rem)'};
+  background-color: ${props => props.toggle && colors.violet9};
+  .burger-line {
+    width: 3rem;
+    height: 0.4rem;
+    margin: 0.2rem 0;
+    background-color: white;
+  }
+  @media screen and (max-width: ${breakpoints.small}) {
+    transform: unset;
+  }
+`;
