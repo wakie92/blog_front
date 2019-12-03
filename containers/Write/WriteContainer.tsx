1ba118f27185d431f14dce85b83c00f4cbf19b15
Changhun Lee <wakie9292@gmail.com>
2019-12-03 22:00:53 +0900

    test function

--- containers/Write/WriteContainer.tsx
+++ containers/Write/WriteContainer.tsx
@@ -3,16 +3,23 @@ import styled from 'styled-components';
 import { Editor, Head, Preview } from '../../components/Write';
 export default function WriteContainer() {
   const [inputValue, setInputValue] = useState('');
+  const [convValue, setConvValue] = useState();
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target;
     setInputValue(value);
   };
+  const handleConv = (html) => {
+    setConvValue(html)
+  }
+  console.log(convValue);
+
   return (
     <>
       <Head />
+      <div dangerouslySetInnerHTML={{ __html: convValue}}></div>
       <EditBox>
         <Editor inputValue={inputValue} handleChange={handleChange} />
-        <Preview inputValue={inputValue} />
+        <Preview inputValue={inputValue} handleConv={handleConv} />
       </EditBox>
     </>
   );
