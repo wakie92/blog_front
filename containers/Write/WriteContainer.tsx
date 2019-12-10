74b0cbbe52cae9c929414f01f31a3f90abf37f62
Changhun Lee <wakie9292@gmail.com>
2019-12-10 22:45:57 +0900

    upload logic

--- containers/Write/WriteContainer.tsx
+++ containers/Write/WriteContainer.tsx
@@ -1,22 +1,45 @@
 import React, { useState } from 'react';
+import { useRouter } from 'next/router'
 import styled from 'styled-components';
+import { useSelector, useDispatch } from 'react-redux';
 import { Editor, Head, Preview } from '../../components/Write';
+import { RootState } from '../../store/modules';
+import { inputMd } from '../../store/modules/post';
+import { ROUTES } from '../../lib/Routes/Routes';
+
 export default function WriteContainer() {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const [title, setTitle] = useState('');
   const [inputValue, setInputValue] = useState('');
   const [convValue, setConvValue] = useState();
+
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const { value } = e.target;
     setInputValue(value);
   };
+
+  const handleTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setTitle(value);
+  }
+
   const handleConv = (html) => {
     setConvValue(html)
   }
-  console.log(convValue);
 
+  const onUpload = () => {
+    const uploadDate = new Date();
+    dispatch(inputMd({
+      title,
+      date: uploadDate,
+      content: convValue
+    }));
+    router.push(ROUTES.devBlog);
+  };
   return (
     <>
-      <Head />
-      <div dangerouslySetInnerHTML={{ __html: convValue}}></div>
+      <Head onUpload={onUpload} title={title} onChange={handleTitle} />
       <EditBox>
         <Editor inputValue={inputValue} handleChange={handleChange} />
         <Preview inputValue={inputValue} handleConv={handleConv} />
