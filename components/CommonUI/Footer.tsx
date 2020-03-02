8eb8d50460f4bc7da9e700a3601c7e4a915ced34
Changhun Lee <wakie9292@gmail.com>
2020-03-02 23:12:07 +0900

    create footer

---
+++ components/CommonUI/Footer.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Footer = () => {
+  return <FooterEle />
+};
+
+const FooterEle = styled.footer`
+  width: 100%;
+  height: 10rem;
+`;
+export default Footer;
