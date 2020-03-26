2cdd1dedde0821c39d4704c3e306dd49be441a78
Changhun Lee <wakie9292@gmail.com>
2020-03-26 21:14:31 +0900

    token check

--- components/CommonUI/Layout.tsx
+++ components/CommonUI/Layout.tsx
@@ -1,7 +1,19 @@
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getIsLogged } from '../../store/modules/loginUI';
 
 export default function Layout(props) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if(props.token) {
+      dispatch(getIsLogged(true))
+    } else {
+      dispatch(getIsLogged(false))
+    }
+  }, [props])
   return (
     <div
       css={css`
