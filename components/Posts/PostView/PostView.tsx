ccf282b55c472832e54dc59a57c0295013fe512a
Changhun <wakie9292@gmail.com>
2020-04-02 18:16:51 +0900

    add renderMarkdown

--- components/Posts/PostView/PostView.tsx
+++ components/Posts/PostView/PostView.tsx
@@ -5,14 +5,15 @@ import { Post } from '../../../store/modules/post';
 import { AsyncState } from '../../../lib/Utils/asyncUtils';
 import { AxiosError } from 'axios';
 import { formatDate } from '../../../lib/Utils/utils';
-// import '../../../lib/styles/github-markdown.css';
-import './prism.css';
+// import '../../../lib/styles/githubMarkdown.css';
+// import '../../../lib/styles/prism.css';
 import Maybe from '../../Maybe/Maybe';
 import Button from '../../CommonUI/Button';
 import { Dispatch, SetStateAction } from 'react';
+import RenderMarkdownView from '../../CommonUI/RenderMarkdownView';
 
 type PostViewProps = {
-  postData: AsyncState<Post, AxiosError>;
+  postData: Post;
 	editMode: boolean;
 	isLogged: boolean;
 	setEditMode: Dispatch<SetStateAction<boolean>>;
@@ -20,14 +21,14 @@ type PostViewProps = {
 };
 
 export default function PostView({ postData, isLogged, setEditMode, editMode, reqDeletePost }: PostViewProps) {
-	const { data, loading, error } = postData;
+	// const { data, loading, error } = postData;
 	console.log(postData);
-	if (loading) return null;
+	// if (loading) return null;
 	return (
 		<Layout>
 			<div className="wrapper">
 				<div className="reg-div">
-					<span className="reg-date">{formatDate(data.date)}</span>
+					<span className="reg-date">{formatDate(postData.date)}</span>
 					<Maybe isVisible={isLogged}>
 						<div className="">
 							<Button 
@@ -42,7 +43,7 @@ export default function PostView({ postData, isLogged, setEditMode, editMode, re
 								type="button" 
 								size="medium" 
 								bgColor={colors.gray1} 
-								onClick={() => reqDeletePost(postData.data.id)}
+								onClick={() => reqDeletePost(postData.id)}
 							>
 								삭제
 							</Button>
@@ -50,13 +51,13 @@ export default function PostView({ postData, isLogged, setEditMode, editMode, re
 					</Maybe>
 				</div>
 
-				<h1>{data.title}</h1>
+				<h1>{postData.title}</h1>
 				<hr />
 				<main>
-					<Maybe isVisible={data.imgUrl}>
-						<img className="main-img" src={data.imgUrl} alt="대표이미지" />
+					<Maybe isVisible={postData.imgUrl}>
+						<img className="main-img" src={postData.imgUrl} alt="대표이미지" />
 					</Maybe>
-					<RenderMarkdown  dangerouslySetInnerHTML={{ __html: data.contentMd }} />
+					<RenderMarkdownView html={postData.rawContent}/>
 				</main>
 			</div>
 		</Layout>
@@ -106,174 +107,3 @@ const Layout = styled.div`
 	@media screen and (max-width: ${breakpoints.medium}) {
 	}
 `;
-
-const RenderMarkdown = styled.div`
-line-height: 2.4rem;
-p, ol, ul, dl {
-	margin-bottom: 1.6rem;
-}
-li {
-	display: list-item;
-	text-align: -webkit-match-parent;
-	list-style-position: inside;
-	list-style-type: disc;
-}
-pre {
-	border: none;
-	margin-bottom: 1.6rem;
-	code {
-		font-size: 1.3rem;
-		border-radius: .8rem;
-		padding: 1.6rem;
-	}
-}
-iframe, img, embed, object, video {
-	max-width: 90%;
-	display: block;
-	margin: 0 auto;
-	margin-top: 4.8rem;
-	margin-bottom: 4.8rem;
-	pointer-events: none;
-}
-blockquote {
-	color: ${colors.gray2};
-	margin-top: 3.2rem;
-	margin-bottom: 3.2rem;
-	padding: 1.6rem 1.5rem 1.6rem 1.5rem;
-	border-left: 0.8rem ${colors.gray5} solid;
-	@media screen and (max-width: ${breakpoints.small}) {
-		font-size: 1.6rem;
-	}
-	:first-child {
-		margin: 0;
-	}
-	:last-child {
-		margin: 0;
-	}
-}
-table {
-	border-collapse: collapse;
-	border-spacing: 0;
-	margin: 1.6rem 0;
-}
-td,
-th {
-	padding: 0
-}
-p,
-ol,
-ul,
-dl {
-	line-height: 2.8rem;
-	margin-bottom: 1.6rem;
-}
-p + p {
-	margin-top: 2.4rem;
-}
-li p {
-	display: inline;
-	line-height: 2.8rem;
-	font-weight: 400;
-}
-ul > li,
-li > ul,
-li > ol {
-	margin-bottom: 0;
-	margin-left: 1.6rem
-}
-ul {
-	list-style-position: inside;
-	list-style-type: disc;
-}
-ol {
-	display: block;
-	list-style-type: decimal;
-	margin-block-start: 1.6rem;
-	margin-block-end: 1.6rem;
-	margin-inline-start: 0;
-	margin-inline-end: 0;
-	padding-inline-start: 4rem;
-}
-p, li {
-	@media screen and (max-width: ${breakpoints.small}) {
-		font-size: 1.6rem;
-	}
-}
-hr {
-	border: 0;
-	border-top: .1rem solid rgba(0, 0, 0, 0.1);
-	border-bottom: .1rem solid ${colors.whiteColor};
-	margin: 1.6rem 0
-}
-a {
-	color: ${colors.gray0};
-	text-decoration: none
-}
-strong {
-	color:  ${colors.defaultBlack};
-}
-h1,
-h2,
-h3,
-h4,
-h5,
-h6 {
-	line-height: 2.43rem;
-	color: ${colors.defaultBlack};
-	font-weight: 700;
-	margin-top: 3.6rem;
-	margin-bottom: 1.6rem;
-	& code {
-		font-weight: 600;
-	}
-}
-h1 a,
-h2 a,
-h3 a,
-h4 a,
-h5 a,
-h6 a {
-	color:  ${colors.gray0};
-}
-h1,
-h2,
-h3,
-h4,
-h5,
-h6,
-p {
-	code {
-		font-size: 1.3rem;
-		padding: .326rem .39rem;
-		border-radius: .3rem;
-		background-color: rgba(27, 31, 35, .05);
-		border: .1rem solid ${colors.markdownCodeColor};
-		color: ${colors.markdownCodeColor};
-		margin: 0 .39rem;
-	}
-}
-h2{
-	margin-top: 7rem;
-	margin-bottom: 3rem;
-	font-size: 3.4rem;
-	line-height: 4.4rem;
-}
-h3{
-	font-size: 2.1rem;
-	margin-top: 4.6rem;
-	margin-bottom: 2rem;
-}
-tr {
-	border-top: .1rem solid ${colors.whiteL1};
-	background: ${colors.whiteColor};
-}
-th,
-td {
-	padding: .6rem 1.3rem;
-	border: .1rem solid ${colors.whiteL2};
-}
-table tr:nth-child(2n) {
-	background: ${colors.whiteL3};;
-}
-
-`
