9eee68ee47df639492f7ef4f95aeface7d072cff
Changhun <wakie9292@gmail.com>
2020-04-02 18:14:22 +0900

    codeBlock higlighting

---
+++ components/CommonUI/CodeHighlighter.tsx
@@ -0,0 +1,19 @@
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import prismThemes from '../../lib/styles/prismThemes';
+import atomOneDark from '../../lib/styles/AtomOneDark';
+
+type CodeHighlighterProps = {
+  value: string;
+  language: string;
+  codeEl?: HTMLElement;
+}
+const CodeHighlighter = ({ value, language }: CodeHighlighterProps) => {
+  console.log(value, language);
+  return (
+    <SyntaxHighlighter language={language} style={atomOneDark}>
+      {value}
+    </SyntaxHighlighter>
+  );
+};
+
+export default CodeHighlighter;
