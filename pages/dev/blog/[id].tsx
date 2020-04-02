5038ad7f5145ccba334f6053dfe0b8e2e11be6fa
Changhun <wakie9292@gmail.com>
2020-04-02 18:19:35 +0900

    ssr

--- pages/dev/blog/[id].tsx
+++ pages/dev/blog/[id].tsx
@@ -11,25 +11,27 @@ import BlogPostContainer from '../../../containers/BlogPost/BlogPostContainer';
 import { NextPageCustom } from '../../../lib/types/nextCustomTypes';
 import { setInputValues } from '../../../store/modules/postUI';
 import { RootState } from '../../../store/modules';
+import { useSelector } from 'react-redux';
 
-type blogType = {
-  postData: AsyncState<Post, AxiosError>;
-  resId: string;
-};
-const PostComponent: NextPageCustom = ({ postData, resId }: blogType) => {
+type blogType = {};
+const PostComponent: NextPageCustom = ({}: blogType) => {
   const [editMode, setEditMode] = useState<boolean>(false);
- console.log(resId);
+  const { postData } = useSelector(( state : RootState ) => ({
+    postData: state.post.post
+  }))
+  const { data } = postData;
+  console.log(data.res);
   return (
     <>
       <Head>
-        <title>{postData.data.title}</title>
+        <title>{data.res.title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Maybe isVisible={editMode}>
+      {/* <Maybe isVisible={editMode}>
         <EditContainer resId={resId} postData={postData} editMode={editMode}/>
-      </Maybe>
+      </Maybe> */}
       <Maybe isVisible={!editMode}>
-        <BlogPostContainer resId={resId} postData={postData} editMode={editMode} setEditMode={setEditMode} />
+        <BlogPostContainer editMode={editMode} setEditMode={setEditMode} />
       </Maybe>
     </>
   );
@@ -53,18 +55,14 @@ PostComponent.getInitialProps = async ({ store, isServer, query  }) => {
     //   tag: '', 
     // }));
   }
-  try {
-    const res = await GetPost(Number(id));
-    console.log('******')
-    console.log(posProps);
-    console.log('******')
-    resId = res.resId;
-    console.log(res);
-    postData = asyncState.success(res.res);
-  } catch (error) {
-    postData = asyncState.error(error);
-  }
-  return { postData, resId };
+  // try {
+  //   const res = await GetPost(Number(id));
+  //   resId = res.resId;
+  //   postData = asyncState.success(res.res);
+  // } catch (error) {
+  //   postData = asyncState.error(error);
+  // }
+  return { };
 };
 
 export default PostComponent;
