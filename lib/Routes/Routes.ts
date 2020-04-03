43e6bcd495cf028426e48ecccd31edf009be732d
Changhun <wakie9292@gmail.com>
2020-04-03 23:15:28 +0900

    Add Enum

--- lib/Routes/Routes.ts
+++ lib/Routes/Routes.ts
@@ -2,5 +2,14 @@ export const ROUTES = {
   about: '/about',
   home: '/',
   devBlog: '/dev/blog',
+  editBlog: '/dev/edit',
   write: '/write',
+}
+
+export enum PATH {
+  ABOUT = 'about',
+  HOME = 'home',
+  DEV_BLOG = 'devBlog',
+  EDIT_BLOG = 'editBlog',
+  WRITE = 'write',
 }
