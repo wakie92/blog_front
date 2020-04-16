ac34da62a268d25de5eee793305e4c25c12f7fa9
Changhun <wakie9292@gmail.com>
2020-04-16 14:02:24 +0900

    port changed

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 const next = require('next');
 const routes = require('./routes');
-const port = parseInt(process.env.PORT) || 3000;
+const port = 8091;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = routes.getRequestHandler(app);
